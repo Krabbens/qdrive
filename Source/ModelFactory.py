a89deb9a26b694cc6cdf4fe467b8c4bfa9df49e9
Krabbens <50266271+Krabbens@users.noreply.github.com>
2023-11-26 04:24:22 +0100

    Add ModelFactory class

---
+++ Source/ModelFactory.py
@@ -0,0 +1,8 @@
+from Source.ThreadWrapper import ThreadWrapper
+
+from Source.Models.DriveFileList import DriveFileList
+
+class ModelFactory(ThreadWrapper):
+    def __init__(self):
+        super().__init__()
+        self.file_list = DriveFileList()
