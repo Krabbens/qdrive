10b6b5168f208d063198481d0121722120b0b800
Krabbens <50266271+Krabbens@users.noreply.github.com>
2023-11-27 17:54:57 +0100

    Add Debug class for logging debug, info, warning, and error messages

---
+++ Source/Debug.py
@@ -0,0 +1,43 @@
+import colorama
+import sys
+import inspect
+
+
+class Debug:
+    def __init__(self, level=0):
+        self.debug = len(sys.argv) > 1 and sys.argv[1] == "--debug"
+        self.colors = [
+            colorama.Fore.LIGHTRED_EX,
+            colorama.Fore.RED,
+            colorama.Fore.BLUE,
+            colorama.Fore.GREEN,
+            colorama.Fore.RESET,
+        ]
+        self.level = level
+        self.levels = [
+            colorama.Fore.LIGHTRED_EX + "[debug] ",
+            colorama.Fore.LIGHTBLUE_EX + "[info] ",
+            colorama.Fore.LIGHTYELLOW_EX + "[warning] ",
+            colorama.Fore.RED + "[error] ",
+        ]
+
+    def __call__(self, *args, **kwargs):
+        if self.debug:
+            stack = inspect.stack()
+            caller_cls = stack[1][0].f_locals["self"].__class__.__name__
+            caller_method = stack[1][0].f_code.co_name
+            print(
+                self.levels[self.level]
+                + self.colors[0]
+                + caller_cls
+                + self.colors[-1]
+                + "."
+                + self.colors[1]
+                + caller_method
+                + " ",
+                end="",
+            )
+            for i in range(len(args) - 1):
+                print(self.colors[i + 2] + args[i], end="")
+            print(self.colors[-1] + args[-1], end="")
+            print(colorama.Fore.RESET)
