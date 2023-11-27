25bd4323b13ed7e9914ede81f76d90f0c2c3cb84
Krabbens <50266271+Krabbens@users.noreply.github.com>
2023-11-27 06:30:51 +0100

    Add open_directory_callback and download_file_async methods

--- Source/Callback.py
+++ Source/Callback.py
@@ -30,9 +30,32 @@ class Callback(TW):
     
     def open_directory_callback(self, files):
         self.conn.toggle_loader()
+        if "parentName" not in files[0]:
+            self.conn.set_current_directory_text("All Files")
+        else:
+            self.conn.set_current_directory_text(files[0]["parentName"])
         self.mf.file_list.clear_items()
         self.mf.file_list.add_items(files)
+
+    @pyqtSlot(str, str, str)
+    def download_file_async(self, path, id, name):
+        # here insert item delegate loader
+        newPath = path.split('file:///')[1] + name
+        self.download_file(newPath, id)
+
+    def download_file_worker(self, path, id):
+        def callback(progress, total):
+            print("Progress: " + str(progress) + " Total: " + str(total))
+        self.program.driveHandler.download_file(path, id, callback)
+        return path
+    
+    def download_file_callback(self, path):
+        # here remove item delegate loader
+        print("Downloaded: " + path)
 
     
     @TW.future(target=open_directory_worker, callback=open_directory_callback)
     def open_directory(self, name): pass
+
+    @TW.future(target=download_file_worker, callback=download_file_callback)
+    def download_file(self, path, id): pass
