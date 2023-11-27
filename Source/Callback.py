950ae435f9422c8253d2209f8764f73185529398
Krabbens <50266271+Krabbens@users.noreply.github.com>
2023-11-27 02:52:13 +0100

    Refactor Callback class and add open_directory_async method

--- Source/Callback.py
+++ Source/Callback.py
@@ -2,25 +2,37 @@ from PyQt5.QtCore import QCoreApplication, QUrl, pyqtSignal, QObject
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtQml import QQmlApplicationEngine
 from PyQt5.QtQml import qmlRegisterType, QQmlComponent
-from Source.Thread import Thread
+from Source.ThreadWrapper import ThreadWrapper as TW
 from concurrent.futures import ThreadPoolExecutor as cf
 import os
 
-class Callback(QObject):
+class Callback(TW):
     def __init__(self, program):
-        QObject.__init__(self)
+        super().__init__()
         self.program = program
+        self.conn = program.connector
+        self.mf = program.modelFactory
         self.engine = None
-        self.threads = []
         self.threadId = 0
 
-    def ManageThread(self, id):
-        for pair in self.threads:
-            if pair[0] == id:
-                pair[1].terminate()
-                self.threads.remove(pair)
-                break
-
     @pyqtSlot()
-    def Exit(self):
+    def exit(self):
         os._exit(0)
+
+    @pyqtSlot(str)
+    def open_directory_async(self, name):
+        self.conn.toggle_loader()
+        self.open_directory(name)
+
+    def open_directory_worker(self, name):
+        files = self.program.driveHandler.open_directory(name)
+        return files
+    
+    def open_directory_callback(self, files):
+        self.conn.toggle_loader()
+        self.mf.file_list.clear_items()
+        self.mf.file_list.add_items(files)
+
+    
+    @TW.future(target=open_directory_worker, callback=open_directory_callback)
+    def open_directory(self, name): pass
