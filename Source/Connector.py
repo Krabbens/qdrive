2a645e1a1e2753673e326092ab8fc0476de789b9
Krabbens <50266271+Krabbens@users.noreply.github.com>
2023-12-03 18:19:13 +0100

    Refactor Connector class and add ThreadEscape functionality

--- Source/Connector.py
+++ Source/Connector.py
@@ -1,8 +1,11 @@
-from PyQt5 import QtCore, QtWidgets, QtQml
+from PyQt5 import QtCore
+from PyQt5.QtCore import QThread
+from Source.Debug import Debug
+from Source.ThreadEscape import ThreadEscape as TE
 
-class Connector(QtCore.QObject):
+class Connector(TE):
     def __init__(self, program, parent=None):
-        super(Connector, self).__init__(parent)
+        super().__init__()
         self.program = program
 
     def init(self):
@@ -14,5 +17,7 @@ class Connector(QtCore.QObject):
     def set_current_directory_text(self, text):
         self.root.setCurrentDirectoryText(text)
 
+    @TE.escape_thread
     def set_gradient_in_delegate(self, index, color):
+        Debug()(QThread.currentThread(), int(QThread.currentThreadId()))
         self.root.setGradientInDelegate(index, color)
