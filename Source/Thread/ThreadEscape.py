6c2406d6b56a843efc63313a83026315a42b20be
Krabbens <50266271+Krabbens@users.noreply.github.com>
2023-12-03 23:06:50 +0100

    Add SignalWrapper class and modify ThreadEscape to use SignalWrapper

--- Source/Thread/ThreadEscape.py
+++ Source/Thread/ThreadEscape.py
@@ -1,19 +1,24 @@
 from PyQt5.QtCore import Qt, pyqtSignal, QObject
 from PyQt5.QtCore import QThread
 from Source.Debug import Debug
-        
+from Source.Thread.SignalWrapper import SignalWrapper as SW
+
+MAX_THREADS = 1000
 
 class ThreadEscape(QObject):
-    threadSignal = pyqtSignal(object, object)
+    threadSignals = [SW() for i in range(MAX_THREADS)]
 
     def __init__(self):
         QObject.__init__(self)
-        self.threadSignal.connect(self.dispatch)
+        for i in range(MAX_THREADS):
+            self.threadSignals[i].threadSignal.connect(self.dispatch)
 
     def dispatch(self, func, *args):
         func(self, *(args[0]))
 
     def escape_thread(func):
         def decorator(self, *args, **kwargs):
-            self.threadSignal.emit(func, args)
+            Debug()("Run:", func.__name__)
+            id = int(QThread.currentThreadId()) % MAX_THREADS
+            self.threadSignals[id].threadSignal.emit(func, args)
         return decorator
