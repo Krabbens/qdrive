100a77924db4ba9ac1fd377d5e8f875965ce12cd
Krabbens <50266271+Krabbens@users.noreply.github.com>
2023-12-04 04:08:38 +0100

    Add import statement for json TODO implement upload_files_async method

--- Source/Callback.py
+++ Source/Callback.py
@@ -7,6 +7,7 @@ from Source.Thread.ThreadWrapper import ThreadWrapper as TW
 from Source.Drive.MonkeyPatch import *
 from concurrent.futures import ThreadPoolExecutor as cf
 import os
+import json
 
 class Callback(TW):
     def __init__(self, program):
@@ -20,6 +21,7 @@ class Callback(TW):
     @pyqtSlot()
     def exit(self):
         os._exit(0)
+
 
     @pyqtSlot(str)
     def open_directory_async(self, name):
@@ -39,18 +41,16 @@ class Callback(TW):
         self.mf.file_list.clear_items()
         self.mf.file_list.add_items(files)
 
+
     @pyqtSlot(int)
     def download_file_async(self, index):
-        # here add item delegate loader
         file = self.mf.file_list.get(index)
         self.download_file(file, index)
 
     def download_file_worker(self, file, index):
         earlier_progress = 0
         def callback(progress, total):
-            # TODO write signal to update progress bar
-            Debug()("Progress: " + str(progress) + " Total: " + str(total))
-            self.conn.set_gradient_in_delegate(index, round((earlier_progress + progress)/total, 2))
+            self.conn.set_download_progress(index, round((earlier_progress + progress)/total, 2))
         if "parts" in file:
             name = file["name"]
             if os.path.exists("./Downloads/" + name):
@@ -74,9 +74,20 @@ class Callback(TW):
         return {"path" : name, "index" : index}
     
     def download_file_callback(self, return_val):
-        # here remove item delegate loader
-        self.conn.set_gradient_in_delegate(return_val["index"], 1)
+        self.conn.set_download_progress(return_val["index"], 1)
         Debug()("Downloaded file:", return_val["path"])
+
+
+    @pyqtSlot(str)
+    def upload_files_async(self, paths):
+        for path in json.loads(paths):
+            self.upload_files(path)
+
+    def upload_files_worker(self, path):
+        Debug()("Uploading file:", path)
+        
+        #self.program.driveHandler.upload_file(path)
+        
 
     
     @TW.future(target=open_directory_worker, callback=open_directory_callback)
@@ -84,3 +95,6 @@ class Callback(TW):
 
     @TW.future(target=download_file_worker, callback=download_file_callback)
     def download_file(self, file, index): pass
+
+    @TW.future(target=upload_files_worker)
+    def upload_files(self, path): pass
